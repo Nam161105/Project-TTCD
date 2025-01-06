e35ace314283f5d6e164d6b379f5c306de2d92d2
Dinh Phuong Nam <dinhphuongnam12384@gmail.com>
2025-01-06 13:12:50 +0700

    add board Tutorial

--- Assets/00 SCRIPTS/UI/MainMenu.cs
+++ Assets/00 SCRIPTS/UI/MainMenu.cs
@@ -5,9 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    [SerializeField] protected GameObject _boardTutorial;
     public void LoadMap()
     {
         SceneLoading.Instance.SceneLoad(1);
+    }
+
+    public void Tutorial()
+    {
+        _boardTutorial.SetActive(true);
+    }
+
+    public void Cancel()
+    {
+        _boardTutorial.SetActive(false);
     }
 
     public void Exit()
